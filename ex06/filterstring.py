7c40de36e4c7b744fea8155bf430da3244d8800d
nolan <firesocietygames@gmail.com>
2026-06-03 17:09:37 +0200

    added ft_filter from ex06

---
+++ ex06/filterstring.py
@@ -0,0 +1,4 @@
+ages = [5, 12, 17, 18, 24, 32]
+
+for i in ages:
+	print(i)
---
+++ ex06/ft_filter.py
@@ -0,0 +1,14 @@
+def ft_filter(func: callable, iterable: list):
+    """filter(function or None, iterable) --> filter object
+
+Return an iterator yielding those items of iterable for which function(item)
+is true. If function is None, return the items that are true."""
+
+    new_iterable = []
+    for value in iterable:
+        try:
+            if func and func(value) or value:
+                new_iterable.append(value)
+        except:
+            continue
+    return new_iterable
