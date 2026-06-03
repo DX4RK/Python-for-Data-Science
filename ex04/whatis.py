98ad101b2cbe83b4f5c84c382901deeebd7ca096
nolan <firesocietygames@gmail.com>
2026-06-03 16:22:27 +0200

    replaced tabs identations to spaces

--- ex04/whatis.py
+++ ex04/whatis.py
@@ -1,24 +1,25 @@
 import sys
 
+
 def main():
-	assert len(sys.argv) <= 2, "more than one argument is provided"
+    assert len(sys.argv) <= 2, "more than one argument is provided"
 
-	num_arg = sys.argv[1]
-	if num_arg == False: return;
+    num_arg = sys.argv[1]
+    if num_arg == False: return;
 
-	try:
-		num = int(num_arg)
-	except ValueError:
-		raise AssertionError("argument is not an integer")
+    try:
+        num = int(num_arg)
+    except ValueError:
+        raise AssertionError("argument is not an integer")
 
-	if num % 2 == 0:
-		print("I'm Even.")
-	else:
-		print("I'm Odd")
+    if num % 2 == 0:
+        print("I'm Even.")
+    else:
+        print("I'm Odd")
 
 if __name__ == '__main__':
-	try:
-		main()
-	except AssertionError as error:
-		print(f"AssertionError: {error}")
+    try:
+        main()
+    except AssertionError as error:
+        print(f"AssertionError: {error}")
 
