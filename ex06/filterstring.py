d4ff1da6a061c1f254fd36db0fc3607cfdaaaeaf
nolan <firesocietygames@gmail.com>
2026-06-04 15:22:44 +0200

    removed for loop to use ft_filter instead

--- ex06/filterstring.py
+++ ex06/filterstring.py
@@ -1,4 +1,5 @@
 import sys
+from ft_filter import ft_filter
 
 def is_str(string: str) -> bool:
     """Check if given argument is a str"""
@@ -25,17 +26,13 @@ def parse_arguments() -> bool:
         is_str(sys.argv[1]) and count_special(sys.argv[1]) == 0 and\
         str_is_int(sys.argv[2])
 
+def is_valid(word: str) -> bool:
+    return len(word) >= int(sys.argv[2])
+
 def main():
     assert parse_arguments(), "the arguments are bad"
-
-    new_list = []
     target_text = sys.argv[1]
-    max_length = int(sys.argv[2])
-
-    for value in target_text.split():
-        if len(value) >= max_length:
-            new_list.append(value)
-    print(new_list)
+    print(ft_filter(is_valid, target_text.split()))
 
 if __name__ == '__main__':
     try:
