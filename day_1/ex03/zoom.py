1fcce2d5aac1ec041142e2f136f3813157749dff
nolan <firesocietygames@gmail.com>
2026-06-16 10:31:55 +0200

    finished ex03

--- day_1/ex03/zoom.py
+++ day_1/ex03/zoom.py
@@ -1,27 +1,22 @@
+from PIL import Image
 from load_image import ft_load
+import matplotlib.pyplot as plt
 import numpy as np
 
 def main():
     image_path = "animal.jpeg"
-    image, pixels, pixels_array = ft_load(image_path)
-    print(pixels_array)
+    image = ft_load(image_path)
+    if image is None:
+        return
+    print(image)
 
-    height, width, channels = pixels.shape
-    start_y = (height - 400) // 2
-    start_x = (width - 400) // 2
+    zoomed_img = image[100:500, 450:850, 0:1]
 
-    zoom = pixels[start_y:start_y + 400, start_x:start_x + 400]
-    zoom_pixels = []
+    print(f"New shape after slicing: {zoomed_img.shape} or {zoomed_img.shape[:2]}")
+    print(zoomed_img)
 
-    for y in range(height):
-        for x in range(width):
-            zoom_pixels.append(
-                image.getpixel((x, y))
-            )
-    zoom_pixels_array = np.array(zoom_pixels)
-
-    print(f"New shape after slicing: {zoom.shape}")
-    print(zoom_pixels_array)
+    plt.imshow(zoomed_img, cmap="gray")
+    plt.show()
 
 if __name__ == "__main__":
     main()
