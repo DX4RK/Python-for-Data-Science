7c9c49d6464caf018ce57e01a8e5dc501ac77733
nolan <firesocietygames@gmail.com>
2026-06-16 10:31:46 +0200

    simplified ex

--- day_1/ex02/load_image.py
+++ day_1/ex02/load_image.py
@@ -2,34 +2,17 @@ import PIL
 from PIL import Image
 import numpy as np
 
-
-def check_file(path: str) -> PIL.Image:
-    """Check if file path really exist."""
-    try:
-        image = Image.open(path)
-    except FileNotFoundError:
-        return
-    return image
-
-
 def ft_load(path: str) -> np.ndarray:
-    """"Load an image from path, print its shape, and return its pixels."""
-    image = check_file(path)
+    """Load an image from path, print its shape, and return its pixels in RGB."""
+    try:
+        image = Image.open(path).convert("RGB")
+    except FileNotFoundError:
+        print(f"Error: file '{path}' not found.")
+        return None
+    except Exception as e:
+        print(f"Error: could not load image: {e}")
+        return None
 
-    if not image:
-        print("Image not found.")
-        return
-
-    pixels_list = []
-    width, height = image.size
-
-    for y in range(height):
-        for x in range(width):
-            pixels_list.append(
-                image.getpixel((x, y))
-            )
     pixels = np.array(image)
-    pixels_array = np.array(pixels_list)
-
-    print(f"The shape of image {pixels.shape}")
-    return pixels_array
+    print(f"The shape of image is: {pixels.shape}")
+    return pixels
